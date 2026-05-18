b05353cdaca4c9abd874152f71a98a631ef50e9a
gusemmanuel23-webdev <gusemmanuel23@gmail.com>
2026-05-18 06:12:23 -0300

    feat: setup inicial del proyecto funcional y configuracion de base de datos

---
+++ src/test/java/com/taller/sanmartin/SanmartinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taller.sanmartin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanmartinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
