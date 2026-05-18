4b03832157bec64c9d1c7df0036898b0948a5d1e
gusemmanuel23-webdev <gusemmanuel23@gmail.com>
2026-05-18 06:41:20 -0300

    style: crear estructura base de paquetes para arquitectura de 3 capas

--- src/main/java/com/taller/sanmartin/SanmartinApplication.java
+++ src/main/java/com/taller/sanmartin/SanmartinApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SanmartinApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SanmartinApplication.class, args);
 	}
