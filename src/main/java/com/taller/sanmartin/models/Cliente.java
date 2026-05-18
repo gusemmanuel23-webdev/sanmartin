07c659833e31ee71603396365945351aab4b503c
gusemmanuel23-webdev <gusemmanuel23@gmail.com>
2026-05-18 06:52:10 -0300

    feat: crear entidad JPA Cliente con mapeo a MySQL y anotaciones Lombok

---
+++ src/main/java/com/taller/sanmartin/models/Cliente.java
@@ -0,0 +1,29 @@
+package com.taller.sanmartin.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "clientes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nombre;
+
+    @Column(length = 30)
+    private String telefono;
+}
